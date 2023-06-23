bbb042fd41d10d6e9cbacdc1286801ebb83efe44
Richard Biener <rguenther@suse.de>
2023-06-23 14:09:47 +0200

    narrowing initializers and initializer_constant_valid_p_1
    initializer_constant_valid_p_1 attempts to handle narrowing
    differences and sums but fails to handle when the overall
    value looks like
    
      VIEW_CONVERT_EXPR<long long int>(NON_LVALUE_EXPR <v>
        -  VEC_COND_EXPR < { 0, 0 } == { 0, 0 } , { -1, -1 } , { 0, 0 } > )
    
    where endtype is scalar integer but value is a vector type.
    In this particular case all is good and we recurse since
    two vector lanes is more than 64bits of long long.  But still
    it compares apples and oranges.
    
    Fixed by appropriately also requiring the type of the
    value to be scalar integral.
    
    	* varasm.cc (initializer_constant_valid_p_1): Also
    	constrain the type of value to be scalar integral
    	before dispatching to narrowing_initializer_constant_valid_p.

--- gcc/varasm.cc
+++ gcc/varasm.cc
@@ -4963,6 +4963,7 @@ initializer_constant_valid_p_1 (tree value, tree endtype, tree *cache)
       if (cache && cache[0] == value)
 	return cache[1];
       if (! INTEGRAL_TYPE_P (endtype)
+	  || ! INTEGRAL_TYPE_P (TREE_TYPE (value))
 	  || TYPE_PRECISION (endtype) >= TYPE_PRECISION (TREE_TYPE (value)))
 	{
 	  tree ncache[4] = { NULL_TREE, NULL_TREE, NULL_TREE, NULL_TREE };
@@ -4999,6 +5000,7 @@ initializer_constant_valid_p_1 (tree value, tree endtype, tree *cache)
       if (cache && cache[0] == value)
 	return cache[1];
       if (! INTEGRAL_TYPE_P (endtype)
+	  || ! INTEGRAL_TYPE_P (TREE_TYPE (value))
 	  || TYPE_PRECISION (endtype) >= TYPE_PRECISION (TREE_TYPE (value)))
 	{
 	  tree ncache[4] = { NULL_TREE, NULL_TREE, NULL_TREE, NULL_TREE };
