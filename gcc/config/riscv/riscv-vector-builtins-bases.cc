7a6c14517464aed5937c85e8ae420c0fbbed94cf
XYenChi <oriachiuan@gmail.com>
2023-08-07 12:06:44 +0800

    add eq_attr v07 and TARGET_VECTOR_07

--- gcc/config/riscv/riscv-vector-builtins-bases.cc
+++ gcc/config/riscv/riscv-vector-builtins-bases.cc
@@ -145,6 +145,7 @@ public:
 /* Implements
  * vle.v/vse.v/vlm.v/vsm.v/vlse.v/vsse.v/vluxei.v/vloxei.v/vsuxei.v/vsoxei.v
  * codegen.  */
+ /*RVV 0.7 vsxei*/
 template<bool STORE_P, lst_type LST_TYPE, bool ORDERED_P>
 class loadstore : public function_base
 {
@@ -2117,6 +2118,7 @@ static CONSTEXPR const freducop<UNSPEC_ORDERED> vfredosum_obj;
 static CONSTEXPR const reducop<SMAX> vfredmax_obj;
 static CONSTEXPR const reducop<SMIN> vfredmin_obj;
 static CONSTEXPR const widen_freducop<UNSPEC_UNORDERED> vfwredusum_obj;
+static CONSTEXPR const widen_freducop<UNSPEC_UNORDERED> vfwredsum_obj;
 static CONSTEXPR const widen_freducop<UNSPEC_ORDERED> vfwredosum_obj;
 static CONSTEXPR const vmv vmv_x_obj;
 static CONSTEXPR const vmv_s vmv_s_obj;
@@ -2179,6 +2181,12 @@ BASE (vsoxei8)
 BASE (vsoxei16)
 BASE (vsoxei32)
 BASE (vsoxei64)
+/*RVV 0.7*/
+BASE (vsxei8)
+BASE (vsxei16)
+BASE (vsxei32)
+BASE (vsxei64)
+
 BASE (vadd)
 BASE (vsub)
 BASE (vrsub)
@@ -2345,6 +2353,7 @@ BASE (vfredmax)
 BASE (vfredmin)
 BASE (vfwredosum)
 BASE (vfwredusum)
+BASE (vfwredsum)
 BASE (vmv_x)
 BASE (vmv_s)
 BASE (vfmv_f)
