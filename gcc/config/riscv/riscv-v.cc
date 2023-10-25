405b4c84c01d8a6649cff8ef4f10df540acefb4b
Robin Dapp <rdapp@ventanamicro.com>
2023-10-25 22:19:43 +0200

    internal-fn: Add VCOND_MASK_LEN.
    In order to prevent simplification of a COND_OP with degenerate mask
    (CONSTM1_RTX) into just an OP in the presence of length masking this
    patch introduces a length-masked analog to VEC_COND_EXPR:
    IFN_VCOND_MASK_LEN.
    
    It also adds new match patterns that allow the combination of
    unconditional unary, binary and ternay operations with the
    VCOND_MASK_LEN into a conditional operation if the target supports it.
    
    gcc/ChangeLog:
    
    	PR tree-optimization/111760
    
    	* config/riscv/autovec.md (vcond_mask_len_<mode><vm>): Add
    	expander.
    	* config/riscv/riscv-protos.h (enum insn_type): Add.
    	* config/riscv/riscv-v.cc (needs_fp_rounding): Add !pred_mov.
    	* doc/md.texi: Add vcond_mask_len.
    	* gimple-match-exports.cc (maybe_resimplify_conditional_op):
    	Create VCOND_MASK_LEN when length masking.
    	* gimple-match.h (gimple_match_op::gimple_match_op): Always
    	initialize len and bias.
    	* internal-fn.cc (vec_cond_mask_len_direct): Add.
    	(direct_vec_cond_mask_len_optab_supported_p): Add.
    	(internal_fn_len_index): Add VCOND_MASK_LEN.
    	(internal_fn_mask_index): Ditto.
    	* internal-fn.def (VCOND_MASK_LEN): New internal function.
    	* match.pd: Combine unconditional unary, binary and ternary
    	operations into the respective COND_LEN operations.
    	* optabs.def (OPTAB_D): Add vcond_mask_len optab.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/vect/vect-cond-arith-2.c: No vect cost model for
    	riscv_v.

--- gcc/config/riscv/riscv-v.cc
+++ gcc/config/riscv/riscv-v.cc
@@ -3868,7 +3868,8 @@ needs_fp_rounding (unsigned icode, machine_mode mode)
 	 && icode != maybe_code_for_pred_widen (FLOAT, mode)
 	 && icode != maybe_code_for_pred_widen (UNSIGNED_FLOAT, mode)
 	 /* vfsgnj */
-	 && icode != maybe_code_for_pred (UNSPEC_VCOPYSIGN, mode);
+	 && icode != maybe_code_for_pred (UNSPEC_VCOPYSIGN, mode)
+	 && icode != maybe_code_for_pred_mov (mode);
 }
 
 /* Subroutine to expand COND_LEN_* patterns.  */
